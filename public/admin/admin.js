1d63967698663b9839362924730e8ebcc3b7c50c
ReshaniKavindya <124257912+ReshaniKavindya@users.noreply.github.com>
2024-10-09 17:01:50 +1100

    add the function to load all the fundraisers

--- public/admin/admin.js
+++ public/admin/admin.js
@@ -1,3 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
     const apiUrl = "http://localhost:3000/api"; // base API URL
     
+    // Load all fundraisers on page load
+  loadFundraisers();
